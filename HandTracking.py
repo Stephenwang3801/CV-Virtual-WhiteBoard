6cf0b962c8e5ae107823fcf67d1e78d38bb6c397
stephensh.wang <S_wang3801@outlook.com>
2022-01-06 03:48:43 -0500

    Added comments and fixed code formatting

--- HandTracking.py
+++ HandTracking.py
@@ -1,6 +1,5 @@
 import cv2
 import mediapipe as mp
-import time
 
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionConfidence=0.5, trackingConfidence=0.5):
@@ -8,63 +7,51 @@ class handDetector():
         self.maxHands = maxHands
         self.detectionConfidence = detectionConfidence
         self.trackingConfidence = trackingConfidence
-
-        # using the Mediapipe hand tracking solution
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, 1, self.detectionConfidence, self.trackingConfidence)
         self.mpDraw = mp.solutions.drawing_utils
+        # Tip node ids for 5 fingers, thumb to little finger
         self.tipIds = [4, 8, 12, 16, 20]
-
 
     def findHands(self, img, draw=True):
         # Color space conversion
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-
         # Detects hand presence and draws node and connections
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
+                    #drawing
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
         return img
 
-
     def findPosition (self, img, handNumber=0, draw=True):
         self.lmList = []
-
-        #Checking if hand detected
+        # Checking if hand detected
         if self.results.multi_hand_landmarks:
-
-            #outputs for a specific hand
+            # Outputs for a specific hand
             myHand = self.results.multi_hand_landmarks[handNumber]
-
             for id, lm in enumerate(myHand.landmark):
-
                 # converting x,y to integer dimension coords
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-
-
                 self.lmList.append([id, cx, cy])
                 if draw:
                       cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
         return self.lmList
 
-
     def fingersUp(self):
+        # a list of length 5 which holds 0 or 1 determining which finger is held up
         fingers = []
-
-        #Thumb
+        # Checking if thumb is up
         if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0]-1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
-
-        #4 Fingers
+        # Loop through index to little finger
         for id in range(1,5):
             if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
-
         return fingers
--- Painter.py
+++ Painter.py
@@ -1,118 +1,112 @@
 import cv2
-import numpy as np
-import time
 import os
+import numpy as np
 import HandTracking as ht
 
 folderPath = "Header"
 myList = os.listdir(folderPath)
-#print(myList)
 palette = []
-currentColor = (128, 128, 128)
-thickness = 25
-x_prev, y_prev = 0, 0
 canvas = np.zeros((720, 1280, 3), np.uint8)
+pt_size = 15
+thickness = 15
+currentColor = (128, 128, 128)
+x_prev, y_prev = 0, 0
 
+# Reading in color palette overlay
 for imPath in myList:
     image = cv2.imread(f'{folderPath}/{imPath}')
     palette.append(image)
-
-#print(len(palette))
-
+# Setting default to eraser and adjusting sizing
 header = palette[myList.index('eraser.jpg')]
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
+# Creating hand detector
 detector = ht.handDetector(detectionConfidence=0.85)
 
 while True:
-    #Importing image
+    # Importing and flipping image
     ret, img = cap.read()
-    #Flip image to invert the inverted image
     img = cv2.flip(img, 1)
-
-    #Finding hand landmark with module
+    # Finding hand landmark with module
     img = detector.findHands(img)
     lmList = detector.findPosition(img, draw=False)
 
     if len(lmList) != 0:
-        #print(lmList)
-
+        #Collecting x and y coords of index and middle finger
         x_index, y_index = lmList[8][1:3]
         x_middle, y_middle = lmList[12][1:3]
-
-
-        #Check which fingers are up
+        # Check which fingers are up
         fingers = detector.fingersUp()
-        #print(fingers)
-
-        #If selection mode - Two fingers up
+        # If selection mode - Two fingers up
         if fingers[1] == 1 and fingers[2] == 1:
             x_prev, y_prev = 0, 0
             cv2.circle(img, (x_index, y_index), 15, (255, 255, 255), cv2.FILLED)
-            print("Select Mode")
-
+            # Color setting based on x coordinate of index finger
             if y_index < 130:
+                # Eraser
                 if 0 < x_index < 164:
                     header = palette[myList.index('eraser.jpg')]
-                    currentColor = (128, 128, 128)
-
-                elif 165 < x_index < 369:
-                    header = palette[myList.index('black.jpg')]
-                    currentColor = (255, 255, 255)
-
-                elif 370 < x_index < 474:
-                    header = palette[myList.index('white.jpg')]
                     currentColor = (0, 0, 0)
-
+                # White
+                elif 165 < x_index < 369:
+                    header = palette[myList.index('white.jpg')]
+                    currentColor = (255, 255, 255)
+                # Purple
+                elif 370 < x_index < 474:
+                    header = palette[myList.index('purple.jpg')]
+                    currentColor = (235, 23, 94)
+                # Red
                 elif 475 < x_index < 639:
                     header = palette[myList.index('red.jpg')]
                     currentColor = (22, 22, 255)
-
+                # Orange
                 elif 640 < x_index < 799:
                     header = palette[myList.index('orange.jpg')]
                     currentColor = (77, 145, 255)
-
+                # Yellow
                 elif 800 < x_index < 954:
                     header = palette[myList.index('yellow.jpg')]
                     currentColor = (89, 222, 255)
-
+                # Green
                 elif 955 < x_index < 1109:
                     header = palette[myList.index('green.jpg')]
                     currentColor = (87, 217, 126)
-
+                # Blue
                 elif 1110 < x_index < 1280:
                     header = palette[myList.index('blue.jpg')]
                     currentColor = (255, 182, 56)
 
         # If draw mode - Index finger up
         if fingers[1] == 1 and fingers[2] == 0:
-            cv2.circle(img, (x_index, y_index), 15, currentColor, cv2.FILLED)
-            print("Draw Mode")
+            cv2.circle(img, (x_index, y_index), pt_size, currentColor, cv2.FILLED)
             if x_prev == 0 and y_prev == 0:
                 x_prev, y_prev = x_index, y_index
-
-            if currentColor == (128, 128, 128):
-                thickness = 25
+            #Adjusting the thickness and point size between erase and draw
+            if currentColor == (0, 0, 0):
+                thickness = 60
+                pt_size = 30
             else:
                 thickness = 15
-                cv2.line(img, (x_prev, y_prev), (x_index, y_index), currentColor, thickness)
-                cv2.line(canvas, (x_prev, y_prev), (x_index, y_index), currentColor, thickness)
+                pt_size = 15
 
+            # Drawing lines
+            cv2.line(img, (x_prev, y_prev), (x_index, y_index), currentColor, thickness)
+            cv2.line(canvas, (x_prev, y_prev), (x_index, y_index), currentColor, thickness)
             x_prev, y_prev = x_index, y_index
 
+    # Creating a gray image
     imgGray = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
+    # Produces an inverse image which creates black areas on where drawing happens
     _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
     imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    # Adding the image with the inversed image
     img = cv2.bitwise_and(img, imgInv)
+    # Or condition to integrate the colored drawing canvas with the B/W image
     img = cv2.bitwise_or(img, canvas)
-
-
-    #Display the color palette
+    # Display the color palette overlay and screen
     img[0: 130, 0:1280] = header
-
     img = cv2.addWeighted(img, 0.5, canvas, 0.5, 0)
     cv2.imshow("Image", img)
-    cv2.imshow("Canvas", canvas)
     cv2.waitKey(1)
